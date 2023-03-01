ebf64b91ff08dc038bf55ae4cbb310531c7c6779
Gabriel Gayol <gabriel.gayol@gmail.com>
2023-03-01 12:02:55 -0300

    Creada carpeta Angular. Comentarios añadidos al index estático

---
+++ Angular/portfolio-argentina-programa/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MainSectionComponent } from './components/main-section/main-section.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    MainSectionComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/portfolio-argentina-programa/src/app/components/habilidades/habilidades.component.ts
@@ -0,0 +1,10 @@
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-habilidades',
+  templateUrl: './habilidades.component.html',
+  styleUrls: ['./habilidades.component.css']
+})
+export class HabilidadesComponent {
+
+}
